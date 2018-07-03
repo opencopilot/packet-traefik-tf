8f1188669d79b0cf1b67a2f3239c3ca152f3d3b5
Patrick DeVivo <patrick.devivo@gmail.com>
2018-07-03 16:30:03 -0400

    add description to variables

--- variables.tf
+++ variables.tf
@@ -1,24 +1,40 @@
-variable "project_id" {}
-variable "packet_token" {}
+variable "project_id" {
+  description = "The Packet project id this load balancer will be deployed in"
+}
+variable "packet_token" {
+  description = "A Packet API token. Ideally this dependence will be removed when Packet metadata can provide more info"
+}
 
 variable "count" {
   default = 1
+  description = "How many load balancer instances to provision. Coming soon: HA with Packet BGP"
 }
 
 variable "backend_tag" {
   default = "traefik-backend"
+  description = "The tag the sidecar container will look for devices on to route to as backends"
 }
 
-variable "facility" {}
-variable "plan" {}
+variable "facility" {
+  description = "The Packet datacenter to provision in"
+}
+variable "plan" {
+  description = "The Packet plan/server type to provision for this load balancer"
+}
 
 variable "os_version" {
   default = "coreos_stable"
+  description = "OS version, current opions are `coreos_stable` and `ubuntu_16_04`"
 }
 
 variable "hostname" {
   default = ""
+  description = "The hostname of the device"
 }
 
-variable "lets_encrypt_email" {}
-variable "main_domain" {}
+variable "lets_encrypt_email" {
+  description = "The email to use for Let's Encrypt"
+}
+variable "main_domain" {
+  description = "Main domain for traefik <> Let's Encrypt integration"
+}
